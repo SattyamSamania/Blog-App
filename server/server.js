27c792821d3804483bdb910e35a272ac6ad0edf6
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-02-24 20:00:09 +0530

    Add Database Connection in Backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
+
+const DB_CONNECTION = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+  console.log('App Connected to Database');
+    app.listen(PORT, () =>
+      console.log(`Server is running @ : http://localhost:${PORT}`)
+  )}
+  )
+  .catch((error) => console.error(error));
